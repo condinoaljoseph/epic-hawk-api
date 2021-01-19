a34a4fe94c627a61a4d57d2ef8bf245798e5c280
Al Joseph <condinoaljoseph@gmail.com>
2021-01-19 14:49:41 +0800

    :hammer: fixed

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -13,7 +12,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
